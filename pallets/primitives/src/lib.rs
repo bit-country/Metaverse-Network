e9263f125c13e6a61d4e77dc48acb50be0e4e15b
justinphamnz <justin.phamnz@gmail.com>
2020-11-11 16:30:06 +1300

    add primitives for universal usage

---
+++ pallets/primitives/src/lib.rs
@@ -0,0 +1,11 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Decode, Encode};
+use sp_runtime::{
+    generic,
+    traits::{BlakeTwo256, IdentifyAccount, Verify},
+    MultiSignature, RuntimeDebug,
+};
+
+/// Balance of an account.
+pub type Balance = u128;
