735f1f0c032a0159417752f0b66b58053e92763e
justinphamnz <justin.phamnz@gmail.com>
2020-11-11 17:15:06 +1300

    country creation refactor

--- pallets/primitives/src/lib.rs
+++ pallets/primitives/src/lib.rs
@@ -7,5 +7,19 @@ use sp_runtime::{
     MultiSignature, RuntimeDebug,
 };
 
+/// An index to a block.
+pub type BlockNumber = u32;
+/// Alias to 512-bit hash when used in the context of a transaction signature on
+/// the chain.
+pub type Signature = MultiSignature;
+/// Alias to the public key used for this chain, actually a `MultiSigner`. Like
+/// the signature, this also isn't a fixed size when encoded, as different
+/// cryptos have different size public keys.
+pub type AccountPublic = <Signature as Verify>::Signer;
+/// Alias to the opaque account ID type for this chain, actually a
+/// `AccountId32`. This is always 32 bytes.
+pub type AccountId = <AccountPublic as IdentifyAccount>::AccountId;
 /// Balance of an account.
 pub type Balance = u128;
+/// Country Id
+pub type CountryId = u64;
