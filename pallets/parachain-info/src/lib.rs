d87cce09b0c0c8da201765a10d40bbb0cd527bc6
justinphamnz <justin.phamnz@gmail.com>
2021-03-26 13:47:40 +1300

    clean up unused pallets and variables

--- pallets/parachain-info/src/lib.rs
+++ pallets/parachain-info/src/lib.rs
@@ -26,17 +26,17 @@ use cumulus_primitives::ParaId;
 pub trait Config: frame_system::Config {}
 
 impl<T: Config> Get<ParaId> for Module<T> {
-	fn get() -> ParaId {
-		Self::parachain_id()
-	}
+    fn get() -> ParaId {
+        Self::parachain_id()
+    }
 }
 
 decl_storage! {
-	trait Store for Module<T: Config> as ParachainInfo {
-		ParachainId get(fn parachain_id) config(): ParaId = 100.into();
-	}
+    trait Store for Module<T: Config> as ParachainInfo {
+        ParachainId get(fn parachain_id) config(): ParaId = 100.into();
+    }
 }
 
 decl_module! {
-	pub struct Module<T: Config> for enum Call where origin: T::Origin {}
+    pub struct Module<T: Config> for enum Call where origin: T::Origin {}
 }
