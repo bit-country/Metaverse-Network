23343689d0dbc7321d1e63027be413c321543ade
justinphamnz <justin.phamnz@gmail.com>
2021-05-06 14:23:21 +1200

    Update all test cases to match with latest deps. Enable Workflow to check unit tests

--- pallets/tokenization/src/mock.rs
+++ pallets/tokenization/src/mock.rs
@@ -1,54 +1,30 @@
 use crate::{Module, Config};
+use crate as tokenization;
+use super::*;
 use frame_support::{
-    impl_outer_event, impl_outer_origin, impl_outer_dispatch, parameter_types, traits::EnsureOrigin, weights::Weight,
+    construct_runtime, parameter_types, ord_parameter_types, weights::Weight,
+    impl_outer_event, impl_outer_origin, impl_outer_dispatch, traits::EnsureOrigin,
 };
-use frame_system as system;
-use frame_system::EnsureSignedBy;
-use sp_core::{sr25519, Pair, H256};
-use sp_runtime::{
-    testing::Header,
-    traits::{BlakeTwo256, IdentityLookup},
-	Perbill,
-	ModuleId
-};
-use primitives::{CountryId, CurrencyId, Balance};
+use sp_core::H256;
+use sp_runtime::{testing::Header, traits::{IdentityLookup, AccountIdConversion}, ModuleId, Perbill};
+use primitives::{CurrencyId, Amount};
+use frame_system::{EnsureSignedBy, EnsureRoot};
+use frame_support::pallet_prelude::{MaybeSerializeDeserialize, Hooks, GenesisBuild};
+use frame_support::sp_runtime::traits::AtLeast32Bit;
+use orml_traits::parameter_type_with_key;
 
 pub type AccountId = u128;
+pub type AuctionId = u64;
+pub type Balance = u128;
+pub type CountryId = u64;
 pub type BlockNumber = u64;
-pub type Amount = i128;
 
 pub const ALICE: AccountId = 1;
 pub const BOB: AccountId = 2;
 pub const COUNTRY_ID: CountryId = 0;
 pub const COUNTRY_ID_NOT_EXIST: CountryId = 1;
-pub const BCG: CurrencyId = 0;
+pub const NUUM: CurrencyId = 0;
 pub const COUNTRY_FUND: CurrencyId = 1;
-
-#[derive(Clone, Eq, PartialEq)]
-pub struct Runtime;
-
-use crate as tokenization;
-
-impl_outer_origin! {
-	pub enum Origin for Runtime {}
-}
-
-impl_outer_event! {
-	pub enum TestEvent for Runtime {
-		frame_system<T>,
-		tokenization<T>,
-		orml_tokens<T>,
-		pallet_balances<T>,
-		orml_currencies<T>,
-		country<T>,
-	}
-}
-
-impl_outer_dispatch! {
-	pub enum Call for Runtime where origin: Origin {
-		frame_system::System,
-	}
-}
 
 
 // Configure a mock runtime to test the pallet.
@@ -60,118 +36,139 @@ parameter_types! {
 	pub const AvailableBlockRatio: Perbill = Perbill::one();
 }
 
-
 impl frame_system::Config for Runtime {
-	type Origin = Origin;
-	type Index = u64;
-	type BlockNumber = BlockNumber;
-	type Call = Call;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = AccountId;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = TestEvent;
-	type BlockHashCount = BlockHashCount;
-	type MaximumBlockWeight = MaximumBlockWeight;
-	type MaximumBlockLength = MaximumBlockLength;
-	type AvailableBlockRatio = AvailableBlockRatio;
-	type Version = ();
-	type PalletInfo = ();
-	type AccountData = pallet_balances::AccountData<Balance>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = ();
-	type BaseCallFilter = ();
-	type SystemWeightInfo = ();
+    type Origin = Origin;
+    type Index = u64;
+    type BlockNumber = BlockNumber;
+    type Call = Call;
+    type Hash = H256;
+    type Hashing = ::sp_runtime::traits::BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = Event;
+    type BlockHashCount = BlockHashCount;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type DbWeight = ();
+    type BaseCallFilter = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
 }
-
-pub type System = frame_system::Module<Runtime>;
-
-impl orml_tokens::Config for Runtime {
-	type Event = TestEvent;
-	type Balance = Balance;
-	type Amount = Amount;
-	type CurrencyId = CurrencyId;
-	type OnReceived = ();
-	type WeightInfo = ();
-}
-pub type Tokens = orml_tokens::Module<Runtime>;
 
 parameter_types! {
-	pub const ExistentialDeposit: Balance = 1;
+	pub const ExistentialDeposit: u64 = 1;
 }
 
 impl pallet_balances::Config for Runtime {
-	type Balance = Balance;
-	type DustRemoval = ();
-	type Event = TestEvent;
-	type ExistentialDeposit = ExistentialDeposit;
-	type AccountStore = frame_system::Module<Runtime>;
-	type MaxLocks = ();
-	type WeightInfo = ();
+    type Balance = Balance;
+    type Event = Event;
+    type DustRemoval = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type MaxLocks = ();
+    type WeightInfo = ();
 }
-pub type PalletBalances = pallet_balances::Module<Runtime>;
-pub type AdaptedBasicCurrency = orml_currencies::BasicCurrencyAdapter<Runtime, PalletBalances, Amount, BlockNumber>;
+
+parameter_type_with_key! {
+	pub ExistentialDeposits: |_currency_id: CurrencyId| -> Balance {
+		Default::default()
+	};
+}
 
 parameter_types! {
-	pub const GetNativeCurrencyId: CurrencyId = BCG;
+    pub const BitCountryTreasuryModuleId: ModuleId = ModuleId(*b"bit/trsy");
+    pub TreasuryModuleAccount: AccountId = BitCountryTreasuryModuleId::get().into_account();
+    pub const CountryFundModuleId: ModuleId = ModuleId(*b"bit/fund");
+}
+
+impl orml_tokens::Config for Runtime {
+    type Event = Event;
+    type Balance = Balance;
+    type Amount = Amount;
+    type CurrencyId = CurrencyId;
+    type WeightInfo = ();
+    type ExistentialDeposits = ExistentialDeposits;
+    type OnDust = orml_tokens::TransferDust<Runtime, TreasuryModuleAccount>;
+}
+
+pub type AdaptedBasicCurrency = orml_currencies::BasicCurrencyAdapter<Runtime, Balances, Amount, BlockNumber>;
+
+impl Config for Runtime {
+    type Event = Event;
+    type TokenId = u64;
+    type CountryCurrency = Currencies;
+}
+
+parameter_types! {
+	pub const GetNativeCurrencyId: CurrencyId = NUUM;
 }
 
 impl orml_currencies::Config for Runtime {
-	type Event = TestEvent;
-	type MultiCurrency = Tokens;
-	type NativeCurrency = AdaptedBasicCurrency;
-	type GetNativeCurrencyId = GetNativeCurrencyId;
-	type WeightInfo = ();
-}
-pub type Currencies = orml_currencies::Module<Runtime>;
-
-parameter_types! {
-	pub const CountryFundModuleId: ModuleId = ModuleId(*b"bit/fund");
+    type Event = Event;
+    type MultiCurrency = Tokens;
+    type NativeCurrency = AdaptedBasicCurrency;
+    type GetNativeCurrencyId = GetNativeCurrencyId;
+    type WeightInfo = ();
 }
 
-impl Config for Runtime {
-	type Event = TestEvent;
-	type TokenId = u64;
-	type CountryCurrency = Currencies;
+impl country::Config for Runtime {
+    type Event = Event;
+    type ModuleId = CountryFundModuleId;
 }
 
-pub type TokenizationModule = Module<Runtime>;
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
 
-impl country::Config for Runtime{
-	type Event = TestEvent;
-	type ModuleId = CountryFundModuleId;
-}
-
-use frame_system::Call as SystemCall;
+construct_runtime!(
+	pub enum Runtime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic
+	{
+		System: frame_system::{Module, Call, Config, Storage, Event<T>},
+		Balances: pallet_balances::{Module, Call, Storage, Config<T>, Event<T>},
+        CountryModule: country::{Module, Call, Storage, Event<T>},
+        Currencies: orml_currencies::{ Module, Storage, Call, Event<T>},
+        Tokens: orml_tokens::{ Module, Storage, Call, Event<T>},
+        TokenizationModule: tokenization:: {Module, Call, Storage, Event<T>},
+	}
+);
 
 pub struct ExtBuilder;
 
 impl Default for ExtBuilder {
-	fn default() -> Self {
-		ExtBuilder
-	}
+    fn default() -> Self {
+        ExtBuilder
+    }
 }
 
 impl ExtBuilder {
-	pub fn build(self) -> sp_io::TestExternalities {
-		let t = frame_system::GenesisConfig::default()
-			.build_storage::<Runtime>()
-			.unwrap();
+    pub fn build(self) -> sp_io::TestExternalities {
+        let mut t = frame_system::GenesisConfig::default()
+            .build_storage::<Runtime>()
+            .unwrap();
 
-		let mut ext = sp_io::TestExternalities::new(t);
-		ext.execute_with(|| System::set_block_number(1));
-		ext
-	}
+        pallet_balances::GenesisConfig::<Runtime> {
+            balances: vec![(ALICE, 100000)],
+        }
+            .assimilate_storage(&mut t)
+            .unwrap();
+
+        let mut ext = sp_io::TestExternalities::new(t);
+        ext.execute_with(|| System::set_block_number(1));
+        ext
+    }
 }
 
-pub fn last_event() -> TestEvent {
-	frame_system::Module::<Runtime>::events()
-		.pop()
-		.expect("Event expected")
-		.event
+pub fn last_event() -> Event {
+    frame_system::Module::<Runtime>::events()
+        .pop()
+        .expect("Event expected")
+        .event
 }
