8d28d2fbc7ad044bf0ad0d17b9d0a8dd172d3cda
Justin <justin.phamnz@gmail.com>
2021-10-28 17:05:27 +1300

    upgrade github workflow

--- scripts/docker_build_pioneer.sh
+++ scripts/docker_build_pioneer.sh
@@ -4,7 +4,7 @@ set -e
 
 VERSION=$(git rev-parse --short HEAD)
 
-echo $VERSIONdocker push bitcountry/pioneer-collattor-node:
+echo $VERSION docker push bitcountry/pioneer-collattor-node:
 
 docker build -f scripts/Dockerfile_pioneer . -t bitcountry/pioneer-collattor-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
 $VERSION
