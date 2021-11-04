f0f14899c56e90e30275cba61a5f178e01522292
Jerry Ren <jerryjren@gmail.com>
2021-11-05 11:26:46 +1300

    Update benchmarking for mint

--- pallets/nft/src/benchmarking.rs
+++ pallets/nft/src/benchmarking.rs
@@ -37,13 +37,21 @@ use sp_runtime::traits::{AccountIdConversion, StaticLookup, UniqueSaturatedInto}
 pub struct Pallet<T: Config>(crate::Pallet<T>);
 
 const SEED: u32 = 0;
+// pub const CLASS_ID: <Runtime as orml_nft::Config>::ClassId = 0;
 
 fn dollar(d: u32) -> Balance {
 	let d: Balance = d.into();
 	d.saturating_mul(1_000_000_000_000_000_000)
 }
 
+fn funded_account<T: Config>(name: &'static str, index: u32) -> T::AccountId {
+	let caller: T::AccountId = account(name, index, SEED);
+	T::Currency::make_free_balance_be(&caller, dollar(100).unique_saturated_into());
+	caller
+}
+
 benchmarks! {
+
 	// create NFT group
 	create_group{
 	}: _(RawOrigin::Root ,vec![1], vec![1] )
@@ -53,7 +61,13 @@ benchmarks! {
 		let initial_balance = dollar(1000);
 		<T as pallet::Config>::Currency::make_free_balance_be(&caller, initial_balance.unique_saturated_into());
 		crate::Pallet::<T>::create_group(RawOrigin::Root.into(), vec![1],vec![1]);
-	}: _(RawOrigin::Signed(caller),vec![1], 0u32.into(), TokenType::Transferable, CollectionType::Collectable)
+	}: _(RawOrigin::Signed(caller), vec![1], 0u32.into(), TokenType::Transferable, CollectionType::Collectable)
+
+	mint{
+		let caller = funded_account::<T>("caller", 0);
+
+		crate::Pallet::<T>::create_class(RawOrigin::Signed(caller.clone()).into(), vec![1], 0u32.into(), TokenType::Transferable, CollectionType::Collectable);
+	}: _(RawOrigin::Signed(caller), 0u32.into(), vec![1], vec![2], vec![1, 2, 3], 3 )
 }
 
 impl_benchmark_test_suite!(Pallet, crate::benchmarking::tests::new_test_ext(), crate::mock::Test);
