4038f7e5f303e163091b7ea81a9b3ab3fc9c6282
Jerry Ren <jerryjren@gmail.com>
2022-01-12 15:06:06 +1300

    Use dummy docker registry

--- scripts/docker_build_tewai.sh
+++ scripts/docker_build_tewai.sh
@@ -7,4 +7,4 @@ VERSION=$(git rev-parse --short HEAD)
 echo $VERSION
 
 docker build -f scripts/Dockerfile_tewai . -t bitcountry/tewai-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
-docker push bitcountry/tewai-node:$VERSION
+docker push jerryjren/tewai-node:$VERSION
