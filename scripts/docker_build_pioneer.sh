1695484afbab9706bfe15b4e2dd26ef77b5a1817
Jerry Ren <jerryjren@gmail.com>
2022-01-13 20:54:25 +1300

    Revert dummy changes

--- scripts/docker_build_pioneer.sh
+++ scripts/docker_build_pioneer.sh
@@ -6,5 +6,5 @@ VERSION=$(git rev-parse --short HEAD)
 
 echo $VERSION
 
-docker build -f scripts/Dockerfile_pioneer . -t jerryjren/pioneer-collattor-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
-docker push jerryjren/pioneer-collattor-node:$VERSION
+docker build -f scripts/Dockerfile_pioneer . -t bitcountry/pioneer-collattor-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
+docker push bitcountry/pioneer-collattor-node:$VERSION
--- scripts/docker_build_tewai.sh
+++ scripts/docker_build_tewai.sh
@@ -6,5 +6,5 @@ VERSION=$(git rev-parse --short HEAD)
 
 echo $VERSION
 
-docker build -f scripts/Dockerfile_tewai . -t jerryjren/tewai-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
-docker push jerryjren/tewai-node:$VERSION
+docker build -f scripts/Dockerfile_tewai . -t bitcountry/tewai-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
+docker push bitcountry/tewai-node:$VERSION
