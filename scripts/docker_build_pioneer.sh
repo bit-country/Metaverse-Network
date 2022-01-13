d462e793b9b5bc86228f869458b8256c330e72de
Jerry Ren <jerryjren@gmail.com>
2022-01-13 14:12:16 +1300

    Update to dummy registry for pioneer

--- scripts/docker_build_pioneer.sh
+++ scripts/docker_build_pioneer.sh
@@ -4,7 +4,7 @@ set -e
 
 VERSION=$(git rev-parse --short HEAD)
 
-echo $VERSION docker push bitcountry/pioneer-collattor-node:
+echo $VERSION
 
-docker build -f scripts/Dockerfile_pioneer . -t bitcountry/pioneer-collattor-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
-$VERSION
+docker build -f scripts/Dockerfile_pioneer . -t jerryjren/pioneer-collattor-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
+docker push jerryjren/pioneer-collattor-node:$VERSION
--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -6,5 +6,5 @@ VERSION=$(git rev-parse --short HEAD)
 
 echo $VERSION
 
-docker build -f scripts/Dockerfile_dev . -t metaverse/metaverse-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
+docker build -f scripts/Dockerfile_dev . -t jerryjren/metaverse-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
 docker push jerryjren/metaverse-node:$VERSION
