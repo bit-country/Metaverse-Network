99c5dc115ffabd35e5712410ac2f6c14c3d739ce
Aleksandar  Brayanov <aleksandar@chexvisual.co.uk>
2022-04-30 06:35:06 +0100

    replaced non-working weight for updating metaverse listing fees

--- pallets/metaverse/src/lib.rs
+++ pallets/metaverse/src/lib.rs
@@ -606,7 +606,7 @@ pub mod pallet {
 			Ok(().into())
 		}
 
-		#[pallet::weight(T::DbWeight::update_metaverse_listing_fee())]
+		#[pallet::weight(T::WeightInfo::update_metaverse_listing_fee())]
 		pub fn update_metaverse_listing_fee(
 			origin: OriginFor<T>,
 			metaverse_id: MetaverseId,
