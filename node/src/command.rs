d579468f59622b7e147b0ffda6f77b51adf0b8b4
Justin <justin.phamnz@gmail.com>
2022-07-08 17:55:52 +1200

    add pioneer rococo chain spec

--- node/src/command.rs
+++ node/src/command.rs
@@ -62,7 +62,7 @@ fn load_spec(id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, St
 		#[cfg(feature = "with-pioneer-runtime")]
 		"pioneer-dev" => Box::new(chain_spec::pioneer::development_config()),
 		#[cfg(feature = "with-pioneer-runtime")]
-		"pioneer-local" => Box::new(chain_spec::pioneer::local_testnet_config()),
+		"pioneer-roc" => Box::new(chain_spec::pioneer::roc_pioneer_testnet_config()),
 		#[cfg(feature = "with-pioneer-runtime")]
 		"pioneer" => Box::new(chain_spec::pioneer::pioneer_network_config_json()?),
 		#[cfg(feature = "with-continuum-runtime")]
