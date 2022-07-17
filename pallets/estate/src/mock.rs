65275eadadbd982ec907c3710c5a09c8bfe1f722
Aleksandar  Brayanov <aleksandar@chexvisual.co.uk>
2022-07-17 12:00:01 +0100

    fix estate mock

--- pallets/estate/src/mock.rs
+++ pallets/estate/src/mock.rs
@@ -446,7 +446,7 @@ impl NFTTrait<AccountId, Balance> for MockNFTHandler {
 	}
 
 	fn get_total_issuance(class_id: Self::ClassId) -> Result<Self::TokenId, DispatchError> {
-		Ok(10.into())
+		Ok(10u64)
 	}
 }
 
