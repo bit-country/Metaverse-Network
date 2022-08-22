5a2f49b81656ef322434c1d95562dd6558d7ad44
Justin <justin.phamnz@gmail.com>
2022-08-22 22:07:34 +1200

    bump runtime version with minimum listing price logic

--- runtime/pioneer/src/lib.rs
+++ runtime/pioneer/src/lib.rs
@@ -222,7 +222,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("pioneer-runtime"),
 	impl_name: create_runtime_str!("pioneer-runtime"),
 	authoring_version: 1,
-	spec_version: 9,
+	spec_version: 10,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
