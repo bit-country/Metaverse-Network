b54c6bb5d3d8f6fb3dc7b0e580efcb1be5e52c83
Justin <justin.phamnz@gmail.com>
2022-08-25 08:56:43 +1200

    Fix wrong PrecompileSet implementation

--- runtime/common/src/precompile/mod.rs
+++ runtime/common/src/precompile/mod.rs
@@ -15,9 +15,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// mod tests;
-pub mod mock;
-
 use frame_support::log;
 use hex_literal::hex;
 use pallet_evm::{
@@ -33,9 +30,12 @@ use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripe
 use sp_core::H160;
 use sp_std::{collections::btree_set::BTreeSet, fmt::Debug, marker::PhantomData};
 
-pub mod currencies;
-
 pub use crate::precompile::currencies::MultiCurrencyPrecompile;
+
+// mod tests;
+pub mod mock;
+
+pub mod currencies;
 
 pub const ECRECOVER: H160 = H160(hex!("0000000000000000000000000000000000000001"));
 pub const SHA256: H160 = H160(hex!("0000000000000000000000000000000000000002"));
@@ -73,93 +73,13 @@ pub struct AllPrecompiles<R> {
 	_marker: PhantomData<R>,
 }
 
-impl<R> AllPrecompiles<R>
-where
-	R: pallet_evm::Config,
-{
-	pub fn continuum() -> Self {
-		Self {
-			active: BTreeSet::from([
-				ECRECOVER,
-				SHA256,
-				RIPEMD,
-				IDENTITY,
-				MODEXP,
-				BN_ADD,
-				BN_MUL,
-				BN_PAIRING,
-				BLAKE2F,
-				// Non-standard precompile starts with 128
-				ECRECOVER_PUBLICKEY,
-				SHA3_256,
-				SHA3_512,
-				MULTI_CURRENCY,
-			]),
-			_marker: Default::default(),
-		}
-	}
-
-	pub fn pioneer() -> Self {
-		Self {
-			active: BTreeSet::from([
-				ECRECOVER,
-				SHA256,
-				RIPEMD,
-				IDENTITY,
-				MODEXP,
-				BN_ADD,
-				BN_MUL,
-				BN_PAIRING,
-				BLAKE2F,
-				// Non-standard precompile starts with 128
-				ECRECOVER_PUBLICKEY,
-				SHA3_256,
-				SHA3_512,
-				MULTI_CURRENCY,
-			]),
-			_marker: Default::default(),
-		}
-	}
-
-	pub fn metaverse() -> Self {
-		Self {
-			active: BTreeSet::from([
-				ECRECOVER,
-				SHA256,
-				RIPEMD,
-				IDENTITY,
-				MODEXP,
-				BN_ADD,
-				BN_MUL,
-				BN_PAIRING,
-				BLAKE2F,
-				// Non-standard precompile starts with 128
-				ECRECOVER_PUBLICKEY,
-				SHA3_256,
-				SHA3_512,
-				MULTI_CURRENCY,
-			]),
-			_marker: Default::default(),
-		}
-	}
-}
-
-impl<R> PrecompileSet for AllPrecompiles<R>
+impl<R> PrecompileSet for MetaverseNetworkPrecompiles<R>
 where
 	R: pallet_evm::Config,
 	MultiCurrencyPrecompile<R>: Precompile,
 	Dispatch<R>: Precompile,
 {
-	fn execute(&self, handle: &mut impl PrecompileHandle) -> Option<PrecompileResult>
-/*
-    fn execute(
-		&self,
-		address: H160,
-		input: &[u8],
-		target_gas: Option<u64>,
-		context: &Context,
-		is_static: bool,
-	) -> Option<PrecompileResult> */ {
+	fn execute(&self, handle: &mut impl PrecompileHandle) -> Option<PrecompileResult> {
 		let address = handle.code_address();
 		if !self.is_precompile(address) {
 			return None;
@@ -197,74 +117,25 @@ where
 			// Default
 			_ => None,
 		}
-		/*
-			// https://github.com/ethereum/go-ethereum/blob/9357280fce5c5d57111d690a336cca5f89e34da6/core/vm/contracts.go#L83
-			let result = if address == ECRECOVER {
-				Some(ECRecover::execute(input, target_gas, context, is_static))
-			} else if address == SHA256 {
-				Some(Sha256::execute(input, target_gas, context, is_static))
-			} else if address == RIPEMD {
-				Some(Ripemd160::execute(input, target_gas, context, is_static))
-			} else if address == IDENTITY {
-				Some(Identity::execute(input, target_gas, context, is_static))
-			} else if address == MODEXP {
-				if R::config().increase_state_access_gas {
-					Some(Modexp::execute(input, target_gas, context, is_static))
-				} else {
-					Some(IstanbulModexp::execute(input, target_gas, context, is_static))
-				}
-			} else if address == BN_ADD {
-				Some(Bn128Add::execute(input, target_gas, context, is_static))
-			} else if address == BN_MUL {
-				Some(Bn128Mul::execute(input, target_gas, context, is_static))
-			} else if address == BN_PAIRING {
-				Some(Bn128Pairing::execute(input, target_gas, context, is_static))
-			} else if address == BLAKE2F {
-				Some(Blake2F::execute(input, target_gas, context, is_static))
-			}
-			// Non-standard precompile starts with 128
-			else if address == ECRECOVER_PUBLICKEY {
-				Some(ECRecoverPublicKey::execute(input, target_gas, context, is_static))
-			} else if address == SHA3_256 {
-				Some(Sha3FIPS256::execute(input, target_gas, context, is_static))
-			} else if address == SHA3_512 {
-				Some(Sha3FIPS512::execute(input, target_gas, context, is_static))
-			}
-			else {
-				if !pallet_evm::Pallet::<R>::is_contract(&context.caller) {
-					log::debug!(target: "evm", "Caller is not a system contract: {:?}", context.caller);
-					return Some(Err(PrecompileFailure::Revert {
-						exit_status: ExitRevert::Reverted,
-						output: "Caller is not a system contract".into(),
-						cost: target_gas.unwrap_or_default(),
-					}));
-				}
-
-				if address == MULTI_CURRENCY {
-					Some(MultiCurrencyPrecompile::<R>::execute(
-						input, target_gas, context, is_static,
-					))
-				} else {
-					None
-				}
-			};
-
-			log::trace!(target: "evm", "Precompile end, address: {:?}, input: {:?}, target_gas: {:?}, context: {:?}, result: {:?}", address, input, target_gas, context, result);
-			if let Some(Err(PrecompileFailure::Revert { ref output, .. })) = result {
-				log::debug!(target: "evm", "Precompile failed: {:?}", core::str::from_utf8(output));
-			};
-			 result
-		}
-		*/
 	}
 
 	fn is_precompile(&self, address: H160) -> bool {
-		if address != hash(1) && address != hash(2) && address != hash(3) 
-			&& address != hash(4) && address != hash(5) && address != hash(6) 
-			&& address != hash(7) && address != hash(8) && address != hash(9) 
-			&& address != hash(1024) && address != hash(1025) && address != hash(1026) 
-			&& address != hash(1027) && address != MULTI_CURRENCY {
-				return false;
+		if address != hash(1)
+			&& address != hash(2)
+			&& address != hash(3)
+			&& address != hash(4)
+			&& address != hash(5)
+			&& address != hash(6)
+			&& address != hash(7)
+			&& address != hash(8)
+			&& address != hash(9)
+			&& address != hash(1024)
+			&& address != hash(1025)
+			&& address != hash(1026)
+			&& address != hash(1027)
+			&& address != MULTI_CURRENCY
+		{
+			return false;
 		}
 		true
 	}
