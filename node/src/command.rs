8244806a9b5db9055c66a9f649ab9913fa1fd84d
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2022-09-28 22:46:04 +0100

    fixed generate_genesis_block definitons in the node commands

--- node/src/command.rs
+++ node/src/command.rs
@@ -502,7 +502,7 @@ pub fn run() -> sc_cli::Result<()> {
 
 			let spec = load_spec(&params.chain.clone().unwrap_or_default())?;
 			let state_version = Cli::native_runtime_version(&spec).state_version();
-			let block: Block = generate_genesis_block(&spec, state_version)?;
+			let block: Block = generate_genesis_block(spec.as_ref(), state_version)?;
 			let raw_header = block.header().encode();
 			let output_buf = if params.raw {
 				raw_header
@@ -617,7 +617,7 @@ pub fn run() -> sc_cli::Result<()> {
 
 					let state_version = RelayChainCli::native_runtime_version(&config.chain_spec).state_version();
 					let block: Block =
-						generate_genesis_block(&config.chain_spec, state_version).map_err(|e| format!("{:?}", e))?;
+						generate_genesis_block(config.chain_spec.as_ref(), state_version).map_err(|e| format!("{:?}", e))?;
 					let genesis_state = format!("0x{:?}", HexDisplay::from(&block.header().encode()));
 
 					let tokio_handle = config.tokio_handle.clone();
@@ -662,7 +662,7 @@ pub fn run() -> sc_cli::Result<()> {
 
 					let state_version = RelayChainCli::native_runtime_version(&config.chain_spec).state_version();
 					let block: Block =
-						generate_genesis_block(&config.chain_spec, state_version).map_err(|e| format!("{:?}", e))?;
+						generate_genesis_block(config.chain_spec.as_ref(), state_version).map_err(|e| format!("{:?}", e))?;
 					let genesis_state = format!("0x{:?}", HexDisplay::from(&block.header().encode()));
 
 					let tokio_handle = config.tokio_handle.clone();
