e697418c7e1af612b6060b3c3eb67c26e1bfd0dd
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2022-11-13 22:37:57 +0000

    fixed missing weights error and benchmarking workflow run error

--- pallets/nft/src/weights.rs
+++ pallets/nft/src/weights.rs
@@ -48,7 +48,7 @@ use frame_support::{traits::Get, weights::{Weight, constants::RocksDbWeight}};
 use sp_std::marker::PhantomData;
 
 /// Weight functions needed for nft.
-pub trait WeightInfo {	fn create_group() -> Weight;	fn create_class() -> Weight;	fn mint() -> Weight;	fn mint_stackable_nft() -> Weight;	fn transfer() -> Weight;	fn transfer_stackable_nft() -> Weight;	fn transfer_batch() -> Weight;	fn sign_asset() -> Weight;	fn set_hard_limit() -> Weight;	fn withdraw_funds_from_class_fund() -> Weight;}
+pub trait WeightInfo {	fn create_group() -> Weight;	fn create_class() -> Weight;	fn mint() -> Weight;	fn mint_stackable_nft() -> Weight;	fn transfer() -> Weight;	fn transfer_stackable_nft() -> Weight;	fn transfer_batch() -> Weight;	fn sign_asset() -> Weight;	fn set_hard_limit() -> Weight;	fn withdraw_funds_from_class_fund() -> Weight;  fn force_update_total_issuance() -> Weight;}
 
 /// Weights for nft using the for collator node and recommended hardware.
 pub struct SubstrateWeight<T>(PhantomData<T>);
@@ -62,7 +62,8 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {	fn create_grou
 		(52_990_000 as Weight)			.saturating_add(T::DbWeight::get().reads(7 as Weight))			.saturating_add(T::DbWeight::get().writes(6 as Weight))	}	fn sign_asset() -> Weight {
 		(35_153_000 as Weight)			.saturating_add(T::DbWeight::get().reads(4 as Weight))			.saturating_add(T::DbWeight::get().writes(3 as Weight))	}	fn set_hard_limit() -> Weight {
 		(13_786_000 as Weight)			.saturating_add(T::DbWeight::get().reads(1 as Weight))			.saturating_add(T::DbWeight::get().writes(1 as Weight))	}	fn withdraw_funds_from_class_fund() -> Weight {
-		(27_590_000 as Weight)			.saturating_add(T::DbWeight::get().reads(3 as Weight))			.saturating_add(T::DbWeight::get().writes(2 as Weight))	}}
+		(27_590_000 as Weight)			.saturating_add(T::DbWeight::get().reads(3 as Weight))			.saturating_add(T::DbWeight::get().writes(2 as Weight))	}   fn force_update_total_issuance() -> Weight {
+		(12_784_000 as Weight)			.saturating_add(T::DbWeight::get().reads(1 as Weight))			.saturating_add(T::DbWeight::get().writes(1 as Weight))}}
 
 // For backwards compatibility and tests
 impl WeightInfo for () {	fn create_group() -> Weight {
@@ -75,4 +76,5 @@ impl WeightInfo for () {	fn create_group() -> Weight {
 		(52_990_000 as Weight)			.saturating_add(RocksDbWeight::get().reads(7 as Weight))			.saturating_add(RocksDbWeight::get().writes(6 as Weight))	}	fn sign_asset() -> Weight {
 		(35_153_000 as Weight)			.saturating_add(RocksDbWeight::get().reads(4 as Weight))			.saturating_add(RocksDbWeight::get().writes(3 as Weight))	}	fn set_hard_limit() -> Weight {
 		(13_786_000 as Weight)			.saturating_add(RocksDbWeight::get().reads(1 as Weight))			.saturating_add(RocksDbWeight::get().writes(1 as Weight))	}	fn withdraw_funds_from_class_fund() -> Weight {
-		(27_590_000 as Weight)			.saturating_add(RocksDbWeight::get().reads(3 as Weight))			.saturating_add(RocksDbWeight::get().writes(2 as Weight))	}}
+		(27_590_000 as Weight)			.saturating_add(RocksDbWeight::get().reads(3 as Weight))			.saturating_add(RocksDbWeight::get().writes(2 as Weight))	}	fn force_update_total_issuance() -> Weight {
+		(12_784_000 as Weight)			.saturating_add(RocksDbWeight::get().reads(1 as Weight))				.saturating_add(RocksDbWeight::get().writes(1 as Weight))}}
