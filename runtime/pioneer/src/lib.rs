fdb442d7c55c351f6aee9f81c2483ca2748701f2
Justin <justin.phamnz@gmail.com>
2023-01-08 12:50:49 +1300

    Increase spec version of pioneer runtime

--- runtime/pioneer/src/lib.rs
+++ runtime/pioneer/src/lib.rs
@@ -227,7 +227,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("pioneer-runtime"),
 	impl_name: create_runtime_str!("pioneer-runtime"),
 	authoring_version: 1,
-	spec_version: 12,
+	spec_version: 14,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
