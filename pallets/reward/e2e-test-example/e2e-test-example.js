84ecb77e3fda97a8398b0ac8f4d59370a6d7a6b9
Aleksandar  Brayanov <aleksandar@chexvisual.co.uk>
2023-02-09 21:59:01 +0000

    added merkle proof formatting and address decoding to e2.e js test

--- pallets/reward/e2e-test-example/e2e-test-example.js
+++ pallets/reward/e2e-test-example/e2e-test-example.js
@@ -1,6 +1,7 @@
 import { MerkleTree} from 'merkletreejs';
-import {keccak256AsU8a} from '@polkadot/util-crypto';
-import {WalkerImpl, createTupleEncoder, createArrayEncoder, encodeU128, encodeU64, encodeU32} from "@scale-codec/core";
+import {hexToU8a} from '@polkadot/util';
+import {keccak256AsU8a, encodeAddress, decodeAddress} from '@polkadot/util-crypto';
+import {WalkerImpl, createTupleEncoder, createArrayEncoder, encodeU128, encodeU64, encodeU32, encodeStr} from "@scale-codec/core";
 
 /* 
    NOTE:
@@ -9,50 +10,45 @@ import {WalkerImpl, createTupleEncoder, createArrayEncoder, encodeU128, encodeU6
    - Leaves need to be encoded using SCALE in order to be validated by the Metaverse Network blockchain
 */
 
-// (1) Get the whitelist data: pair of AccountId, Balance / (ClassId, TokenId)
-const values = [
-   [2, 10],
-   [3, 25],
-   [4, 50],
-   [5, 75]
-];
-
-
 // SCALE Libraries:
 // https://github.com/paritytech/scale-ts
 // https://github.com/soramitsu/scale-codec-js-library
 
 // Simple example of SCALE encoding for a leaf 
 const bob = [2,10];
-const bob_output = [...WalkerImpl.encode(BigInt(bob[0]), encodeU128), ...WalkerImpl.encode(BigInt(bob[1]), encodeU128)];
-console.log("Bob SCALE: ", bob_output);
+//const bob_address = encodeAddress(WalkerImpl.encode(BigInt(bob[0]), encodeU128));
+//const bob_output_address = [...decodeAddress(bob_address), ...WalkerImpl.encode(BigInt(bob[1]), encodeU128)];
+//console.log("Bob SCALE: ", bob_output);
 
 // TEST DATA (VALID SCALE OUTPUT)
-
 const bob_leaf_array = new Uint8Array([2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-console.log("Is Bob SCALE encoding correct? ", bob_output.toString() === bob_leaf_array.toString());
-
 const charlie_leaf_array = new Uint8Array([3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 const donna_leaf_array = new Uint8Array([4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 const eva_leaf_array = new Uint8Array([5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-const charlie2_leaf_array = new Uint8Array([3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-const donna2_leaf_array = new Uint8Array([4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 501, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-const eva2_leaf_array = new Uint8Array([5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 751, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
 const scale_values_valid = [
    bob_leaf_array, 
    charlie_leaf_array, 
    donna_leaf_array, 
    eva_leaf_array,
-   //charlie2_leaf_array,
-  // donna2_leaf_array
 ];
 
-// (2) Encode data using SCALE
-const scale_values = values.map(x => [...WalkerImpl.encode(BigInt(x[0]), encodeU128), ...WalkerImpl.encode(BigInt(x[1]), encodeU128)])
+// (1) Get the whitelist data: pair of AccountId, Balance / (ClassId, TokenId)
+const values = [
+   ['5FpqLqqbFyYWgYtgQS11HvTkaripk1nPFFti6CwDaMj8cSvu', 10],
+   ['5EUXjqNx3Rsh3wtDJAPBzEvJdGVD3QmxmMUjrfARNr4uh7pq', 25],
+   ['5Dqy8KtwmGJd6Tkar8Va3Uw7xvX4RQAhrygUk3T8vUxDXf2a', 50],
+   ['5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY', 75]
+];
+
+// (2) Decode the address string and encode reward data using SCALE
+const scale_values = values.map(x => [...decodeAddress(x[0]), ...WalkerImpl.encode(BigInt(x[1]), encodeU128)]);
+
+// Alternatively the address can be encoded as string. !!! Need to test and confirm if that would produce valid leaf hashes
+// const scale_values = values.map(x => [...WalkerImpl.encode(x[0], encodeStr), ...WalkerImpl.encode(BigInt(x[1]), encodeU128)]);
 
 // Encoding for NFT-based input
-//const scale_values = values.map(x => [...WalkerImpl.encode(BigInt(x[0]), encodeU128), ...WalkerImpl.encode(BigInt(x[1]), encodeU32)], , ...WalkerImpl.encode(BigInt(x[2]), encodeU64))
+//const scale_values = values.map(x => [...decodeAddress(x[0]), ...WalkerImpl.encode(x[1], encodeU32)], , ...WalkerImpl.encode(BigInt(x[2]), encodeU64))
 
 // (3) Build merkle tree using double hashing for leaves - use it ot call setRewardRoot
 //const tree = StandardMerkleTree.of(values, ["uint256", "uint256"]);
@@ -62,10 +58,10 @@ const tree = new MerkleTree(leaves, keccak256AsU8a);
 console.log("Merkle Tree: ", tree.toString());
 
 // (4) Get proofs for each leaf - use them to call claimRewardRoot
-const bob_proof = tree.getHexProof(leaves[0]);
-const charlie_proof = tree.getHexProof(leaves[1]);
-const donna_proof = tree.getHexProof(leaves[2]);
-const eva_proof = tree.getHexProof(leaves[3]);
+const bob_proof = tree.getHexProof(leaves[0]).map(x => x.toString().substring(2));
+const charlie_proof = tree.getHexProof(leaves[1]).map(x => x.toString().substring(2));
+const donna_proof = tree.getHexProof(leaves[2]).map(x => x.toString().substring(2));
+const eva_proof = tree.getHexProof(leaves[3]).map(x => x.toString().substring(2));
 
 console.log("Bob Proof Array: ", bob_proof);
 console.log("Charlie Proof Array: ", charlie_proof);
