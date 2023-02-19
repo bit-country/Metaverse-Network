687453ad5fae4b20cb0821e04323a186c691450b
Aleksandar  Brayanov <aleksandar@chexvisual.co.uk>
2023-02-19 22:12:24 +0000

    fixed formatting

--- runtime/metaverse/src/weights/module_reward.rs
+++ runtime/metaverse/src/weights/module_reward.rs
@@ -11,86 +11,86 @@ use sp_std::marker::PhantomData;
 pub struct WeightInfo<T>(PhantomData<T>);
 impl<T: frame_system::Config> reward::WeightInfo for WeightInfo<T> {
 	fn create_campaign() -> Weight {
-		(41_707_000 as Weight)
+		(187_900_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(7 as Weight))
 	}
 	fn create_nft_campaign() -> Weight {
-		(43_668_000 as Weight)
+		(106_500_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(6 as Weight))
 			.saturating_add(T::DbWeight::get().writes(8 as Weight))
 	}
 	fn claim_reward() -> Weight {
-		(35_006_000 as Weight)
+		(82_600_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(4 as Weight))
 	}
 	fn claim_reward_root() -> Weight {
-		(42_272_000 as Weight)
+		(189_300_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(6 as Weight))
 			.saturating_add(T::DbWeight::get().writes(4 as Weight))
 	}
 	fn claim_nft_reward() -> Weight {
-		(39_669_000 as Weight)
+		(94_400_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(7 as Weight))
 			.saturating_add(T::DbWeight::get().writes(4 as Weight))
 	}
 	fn claim_nft_reward_root() -> Weight {
-		(44_661_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(8 as Weight))
+		(107_300_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(9 as Weight))
 			.saturating_add(T::DbWeight::get().writes(3 as Weight))
 	}
 	fn set_reward() -> Weight {
-		(21_354_000 as Weight)
+		(47_100_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(3 as Weight))
 	}
 	fn set_reward_root() -> Weight {
-		(22_813_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(5 as Weight))
-			.saturating_add(T::DbWeight::get().writes(4 as Weight))
+		(107_600_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(6 as Weight))
+			.saturating_add(T::DbWeight::get().writes(5 as Weight))
 	}
 	fn set_nft_reward() -> Weight {
-		(22_278_000 as Weight)
+		(61_500_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(3 as Weight))
 	}
 	fn set_nft_reward_root() -> Weight {
-		(22_938_000 as Weight)
+		(61_700_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(4 as Weight))
-			.saturating_add(T::DbWeight::get().writes(4 as Weight))
+			.saturating_add(T::DbWeight::get().writes(5 as Weight))
 	}
 	fn close_campaign() -> Weight {
-		(50_279_000 as Weight)
+		(123_000_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(7 as Weight))
 	}
 	fn close_nft_campaign() -> Weight {
-		(47_338_000 as Weight)
+		(115_900_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(6 as Weight))
-			.saturating_add(T::DbWeight::get().writes(8 as Weight))
+			.saturating_add(T::DbWeight::get().writes(9 as Weight))
 	}
 	fn cancel_campaign() -> Weight {
-		(38_746_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(3 as Weight))
+		(98_800_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(3 as Weight))
 	}
 	fn cancel_nft_campaign() -> Weight {
-		(31_672_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(4 as Weight))
+		(119_800_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(5 as Weight))
 			.saturating_add(T::DbWeight::get().writes(4 as Weight))
 	}
 	fn add_set_reward_origin() -> Weight {
-		(11_374_000 as Weight)
+		(24_200_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(2 as Weight))
 			.saturating_add(T::DbWeight::get().writes(2 as Weight))
 	}
 	fn remove_set_reward_origin() -> Weight {
-		(11_973_000 as Weight)
+		(47_600_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(2 as Weight))
 			.saturating_add(T::DbWeight::get().writes(2 as Weight))
 	}
 	fn on_finalize() -> Weight {
-		(13_598_000 as Weight).saturating_add(T::DbWeight::get().reads(2 as Weight))
+		(29_500_000 as Weight).saturating_add(T::DbWeight::get().reads(2 as Weight))
 	}
 }
