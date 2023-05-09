893d6471c11576454c9935edc13211c867824f63
Aleksandar  Brayanov <aleksandar@chexvisual.co.uk>
2023-05-09 03:40:10 +0100

    updated nft pallet benchmarking

--- runtime/metaverse/src/weights/module_nft.rs
+++ runtime/metaverse/src/weights/module_nft.rs
@@ -11,57 +11,57 @@ use sp_std::marker::PhantomData;
 pub struct WeightInfo<T>(PhantomData<T>);
 impl<T: frame_system::Config> nft::WeightInfo for WeightInfo<T> {
 	fn create_group() -> Weight {
-		(15_762_000 as Weight)
+		(75_638_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(2 as Weight))
 			.saturating_add(T::DbWeight::get().writes(3 as Weight))
 	}
 	fn create_class() -> Weight {
-		(32_238_000 as Weight)
+		(165_972_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(3 as Weight))
 			.saturating_add(T::DbWeight::get().writes(4 as Weight))
 	}
 	fn mint() -> Weight {
-		(49_566_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(5 as Weight))
+		(118_473_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(8 as Weight))
 			.saturating_add(T::DbWeight::get().writes(10 as Weight))
 	}
 	fn mint_stackable_nft() -> Weight {
-		(46_560_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(6 as Weight))
+		(125_085_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(5 as Weight))
 			.saturating_add(T::DbWeight::get().writes(8 as Weight))
 	}
 	fn transfer() -> Weight {
-		(30_588_000 as Weight)
+		(91_449_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(5 as Weight))
 			.saturating_add(T::DbWeight::get().writes(3 as Weight))
 	}
 	fn transfer_stackable_nft() -> Weight {
-		(23_361_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(3 as Weight))
+		(63_643_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(2 as Weight))
 	}
 	fn transfer_batch() -> Weight {
-		(49_633_000 as Weight)
+		(197_726_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(8 as Weight))
 			.saturating_add(T::DbWeight::get().writes(6 as Weight))
 	}
 	fn sign_asset() -> Weight {
-		(34_549_000 as Weight)
+		(129_971_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(4 as Weight))
 			.saturating_add(T::DbWeight::get().writes(3 as Weight))
 	}
 	fn set_hard_limit() -> Weight {
-		(14_025_000 as Weight)
+		(69_228_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(1 as Weight))
 			.saturating_add(T::DbWeight::get().writes(1 as Weight))
 	}
 	fn withdraw_funds_from_class_fund() -> Weight {
-		(27_002_000 as Weight)
+		(70_239_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(3 as Weight))
 			.saturating_add(T::DbWeight::get().writes(2 as Weight))
 	}
 	fn force_update_total_issuance() -> Weight {
-		(12_330_000 as Weight)
+		(60_268_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(1 as Weight))
 			.saturating_add(T::DbWeight::get().writes(1 as Weight))
 	}
