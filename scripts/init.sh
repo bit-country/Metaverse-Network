0eb5a36deea99d34d9bbf9980a4ec9e008e5011f
Justin <justin.phamnz@gmail.com>
2023-07-30 11:26:27 +1200

    Update init permission

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,9 +6,9 @@ echo "*** Initializing WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ]; then
   rustup default nightly && rustup update
-  rustup update nightly-2022-11-15
+  rustup update nightly-2023-01-30
   rustup update stable
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2022-11-15
-rustup default nightly-2022-11-15
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-01-30
+rustup default nightly-2023-01-30
