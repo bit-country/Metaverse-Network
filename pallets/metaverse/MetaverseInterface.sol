01a2dd4f8f2c36a2949e780c581de74e06c166c0
Aleksandar  Brayanov <aleksandar@chexvisual.co.uk>
2023-09-01 15:00:41 +0100

    fixed MetaverseInterface Solidity contract

--- pallets/metaverse/MetaverseInterface.sol
+++ pallets/metaverse/MetaverseInterface.sol
@@ -12,47 +12,53 @@ Metaverse constant METAVERSE_CONTRACT = Metaverse(METAVERSE_PRECOMPILE_ADDRESS);
 /// @custom:address 0x0101010101010101010000000000000000000000
 interface Metaverse {
     /// @dev Gets the owner of the specified metaverse fund address.
-    /// @custom:selector 80a08230
+    /// @custom:selector 31a642be
     /// @param metaverse_id uint256 The ID of the metaverse.
     /// @return The address of the metaverse owner.
     function getMetaverseOwner(uint256 metaverse_id) external view returns (address);
 
     /// @dev Gets the metadata of the specified metaverse fund address.
-    /// @custom:selector 80a08231
+    /// @custom:selector dd58ec4e
     /// @param metaverse_id uint256 The ID of the metaverse.
     /// @return A bytes array representing the metaverse metadata.
-    function getMetaverse(uint256 metaverse_id) external view returns (bytes32);
+    function getMetaverseMetadata(uint256 metaverse_id) external view returns (bytes32);
     
     /// @dev Gets the balance of the specified metaverse fund address.
-    /// @custom:selector 80a08232
+    /// @custom:selector a9c444a8
     /// @param metaverse_id uint256 The ID of the metaverse.
     /// @return An uint256 representing the class fund balance.
     function getMetaverseFundBalance(uint256 metaverse_id) external view returns (uint256);
     
-    /// @dev Create metaverse for a specified address
+    /// @dev Create metaverse
     /// @custom:selector b9059cba
-    /// @param owner address The address that will own the metaverse.
     /// @param metadata bytes The metadata of the metaverse.
     /// @return true if the mint was successful, revert otherwise.
-    function createMetaverse(address owner, bytes32 metadata) external returns (bool);
+    function createMetaverse(bytes32 metadata) external returns (bool);
     
     /// @dev Withdraw from metaverse fund
-    /// @custom:selector b1059cbb
-    /// @param owner The metaverse owner address.
+    /// @custom:selector 34c05f62
     /// @param metaverse_id uint256 The ID of the metaverse.
     /// @return true if the withdraw was successful, revert otherwise.
-    function withdrawFromMetaverseFund(address owner, uint256 metaverse_id) external returns (bool);
+    function withdrawFromMetaverseFund(uint256 metaverse_id) external returns (bool);
     
-    /// @dev Event emitted when a metaverse is created.
-    /// @custom:selector edf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ff
-    /// @param owner address The address that will own the metaverse.
+    /// @dev Transfer metaverse to specified address
+    /// @custom:selector f063c7b9
+    /// @param to address The new metaverse owner address.
     /// @param metaverse_id uint256 The ID of the metaverse.
-    event metaverseCreated(address indexed owner, uint256 metaverse_id);
+    /// @return true if the transfer was successful, revert otherwise.
+    function transferMetaverse(address to, uint256 metaverse_id) external returns (bool);
+
+    /// @dev Event emitted when a metaverse is created.
+    /// @param metaverse_id uint256 The ID of the metaverse.
+    event metaverseCreated(uint256 indexed metaverse_id);
+
+    /// @dev Event emitted when a metaverse is transferred.
+    /// @param metaverse_id uint256 The ID of the metaverse.
+    /// @param owner address The address of the new owner
+    event metaverseTransfered(uint256 indexed metaverse_id, addreess owner);
     
     /// @dev Event emitted when withdraw from metaverse fund is successful.
-    /// @custom:selector bdf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3cf
-    /// @param owner The address sending the tokens
     /// @param metaverse_id uint256 The ID of the metaverse.
     /// @param balance uint256 The amount of tokens transferred.
-    event metaverseFundWithdraw(address indexed owner, uint256 metaverse_id, uint256 balance);
+    event metaverseFundWithdraw(uint256 indexed metaverse_id, uint256 balance);
 }
