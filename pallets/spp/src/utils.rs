655f8d12a291452d13a8c809f54b2692a4b6bc07
Justin <justin.phamnz@gmail.com>
2023-11-24 12:52:59 +1300

    WIP - Update pool info struct property

--- pallets/spp/src/utils.rs
+++ pallets/spp/src/utils.rs
@@ -24,6 +24,7 @@ use sp_runtime::{
 	Permill, RuntimeDebug,
 };
 
+use primitives::bounded::{FractionalRate, Rate};
 use primitives::{FungibleTokenId, PoolId};
 
 // Helper methods to compute the issuance rate for undeployed land.
@@ -32,7 +33,7 @@ use primitives::{FungibleTokenId, PoolId};
 #[derive(Eq, PartialEq, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
 pub struct PoolInfo<AccountId> {
 	pub creator: AccountId,
-	pub commission: Permill,
+	pub commission: Rate,
 	/// Currency id of the pool
 	pub currency_id: FungibleTokenId,
 	/// Max nft rewards
